73396b5b78c7f6a58a3660551cd2a5a8265736d9
liuyonghui <18435162263@163.com>
2026-06-18 03:07:17 +0800

    E

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,15 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
 export default defineConfig({
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+      }
+    }
+  },
   build: {
     rollupOptions: {
       input: {
